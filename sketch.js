71cc44368dd7468d0663a592f4942aee9fa9a6f1
Chetrithasai <87260107+Chetrithasai@users.noreply.github.com>
2022-01-12 08:34:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+function preload(){
+
+}
+
+function setup(){
+  canvas = createCanvas(windowWidth,windowHeight);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+}
